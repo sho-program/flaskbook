975e5104f83b21473ba63d01d1e18f346de5a60b
Aso2001195 <2001195@s.asojuku.ac.jp>
2023-01-30 01:24:07 +0900

    endpoint

--- apps/minimalapp/app.py
+++ apps/minimalapp/app.py
@@ -6,3 +6,8 @@ app = Flask(__name__)
 @app.route("/")
 def index():
     return "Hello Flaskbook"
+
+
+@app.route("/hello", methods=["GET"], endpoint="hello-endpoint")
+def hello():
+    return "Hello world!"
