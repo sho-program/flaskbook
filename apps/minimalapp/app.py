440892ae95c54595344582b99c3e6f9fe06ab6db
Aso2001195 <2001195@s.asojuku.ac.jp>
2023-02-01 01:04:57 +0900

    リクエストコンテキスト

--- apps/minimalapp/app.py
+++ apps/minimalapp/app.py
@@ -1,4 +1,4 @@
-from flask import Flask
+from flask import Flask, current_app, g, render_template, request, url_for
 
 app = Flask(__name__)
 
@@ -8,6 +8,31 @@ def index():
     return "Hello Flaskbook"
 
 
-@app.route("/hello", methods=["GET"], endpoint="hello-endpoint")
-def hello():
-    return "Hello world!"
+@app.route("/hello/<name>", methods=["GET", "POST"], endpoint="hello-endpoint")
+def hello(name):
+    return f"Hello ,{name}"
+
+
+@app.route("/name/<name>")
+def show_name(name):
+    return render_template("index.html", name=name)
+
+
+with app.test_request_context():
+    print(url_for("index"))
+
+    print(url_for("hello-endpoint", name="world"))
+
+    print(url_for("show_name", name="ichiro", page=1))
+
+with app.test_request_context("/users?updated=true"):
+    print(request.args.get("updated"))
+
+
+ctx = app.app_context()
+ctx.push()
+
+print(current_app.name)
+
+g.connection = "connection"
+print(g.connection)
